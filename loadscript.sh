c703ad6b424ae5c3fe3273f4de0d0afdb368e49b
Fabian <fznv6@hotmail.com>
2025-08-10 22:03:11 -0400

    Made the 1 bot only thing work

--- loadscript.sh
+++ loadscript.sh
@@ -3,10 +3,10 @@ cd /home/osu/DiscordBot/GLaDOS/my-venv
 . bin/activate
 
 cd ..
-if [ $(pgrep -f "GLaDOS.py") -lt 1 ]; then
-    python -u GLaDOS.py >> glados.log
-else
+if [ $(pgrep -f "GLaDOS.py") > 0 ]; then
     echo "Glados tried starting but is already running" >> glados.log
     echo "Glados is already running"
+else
+    python -u GLaDOS.py >> glados.log
 fi
 
